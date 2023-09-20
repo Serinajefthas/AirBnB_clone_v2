4d2258e6833d3a6d683a8015592ffddd6606c9d3
Serinajefthas <serina.jefthas@gmail.com>
2023-09-20 13:04:10 -0700

    Update classes to utilise sqlalchemy db storage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenity class that defines class Amenity"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_menities = ''
