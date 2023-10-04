3fe5867dc99ab4d2324ba2a18d5e498bb7f3a15e
Serinajefthas <serina.jefthas@gmail.com>
2023-10-04 11:43:56 -0700

    Add script to setup web servers for web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Script sets up web servers for deploying web static content
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+if [ -L /data/web_static/current ]; then
+    sudo rm -f /data/web_static/current
+fi
+sudo ln -sf /data/web_static/current /data/web_static/releases/test 
+
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/location \/ {/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
