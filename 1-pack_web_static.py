4c75a0c44db4893b9b8c6f5c18dfb96fdf7dba51
Serinajefthas <serina.jefthas@gmail.com>
2023-10-12 01:46:27 -0700

    Add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script generates .tgz archive of /web_static/ contents
+"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Creates compressed tgz archive of files in web_static folder
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    local("mkdir -p versions")
+
+    archive_path = local("tar -cvzf versions/web_static_{}.tgz web_static".format(now))
+    if archive_path.failed:
+        return None
+    else:
+        return archive_path
